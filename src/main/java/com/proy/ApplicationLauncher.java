ec8ca06de292d93d36ac30db5c12c383ec261fff
FernandoVS11 <97609743+FernandoVS11@users.noreply.github.com>
2025-05-16 17:22:27 -0600

    Update ApplicationLauncher.java

--- src/main/java/com/proy/ApplicationLauncher.java
+++ src/main/java/com/proy/ApplicationLauncher.java
@@ -26,32 +26,26 @@ public class ApplicationLauncher {
      *
      * @param args Argumentos de línea de comandos (no utilizados en esta aplicación).
      */
-    public static void main(String[] args) {
-        try (Scanner scanner = new Scanner(System.in)) {
-
-            System.out.println("Bienvenido al programa de control de verrsiones...");
-
-            System.out.println("Archivo Previo:");
-            Path originalProjectPath = UserInputHandler.requestValidPath(scanner);
-
-            System.out.println("Archivo Modificado:");
-            Path modifiedProjectPath = UserInputHandler.requestValidPath(scanner);
-
-            System.out.println("Directorio donde se guardará el reporte de cambios:");
-            Path reportFilePath = UserInputHandler.requestValidPath(scanner);
-
-            ProgramAnalyzer analyzer = new ProgramAnalyzer();
-            analyzer.analyzePrograms(originalProjectPath, modifiedProjectPath, reportFilePath);
-
-        } catch (FileNotFoundException e) {
-            System.out.println(e.getMessage());
-        } catch (Exception e) {
-            System.out.println("Ocurrió un error inesperado.");
-        }
-        System.out.println("\nPresiona Enter para salir...");
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Bienvenido al programa de control de versiones...");
+        System.out.println("Archivo Previo:");
+        Path originalProjectPath = UserInputHandler.requestValidPath(scanner);
+    
+        System.out.println("Archivo Modificado:");
+        Path modifiedProjectPath = UserInputHandler.requestValidPath(scanner);
+    
+        System.out.println("Directorio donde se guardará el reporte de cambios:");
+        Path reportFilePath = UserInputHandler.requestValidPath(scanner);
+    
         try {
-            System.in.read();
-        } catch (Exception ignored) {}
-
+            new ProgramAnalyzer()
+                .analyzePrograms(originalProjectPath, modifiedProjectPath, reportFilePath);
+        } catch (Exception e) {
+            System.out.println("Ocurrió un error inesperado: " + e.getMessage());
+        }
+    
+        System.out.println("\nPresiona Enter para salir...");
+        scanner.nextLine();
     }
-}
+}    
